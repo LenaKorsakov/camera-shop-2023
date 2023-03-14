8c1a7d9ad2f205140fb4cfc4b19013a1c385078e
ElenaKorsakova <lenawwka@gmail.com>
2023-03-14 16:18:44 +0100

    Create cleaning of form and navigation on the product page from search form

--- project/src/components/form-search/form-search.tsx
+++ project/src/components/form-search/form-search.tsx
@@ -1,20 +1,27 @@
-import { ChangeEvent, useEffect, useMemo, useState } from 'react';
+import { ChangeEvent, SyntheticEvent, useMemo, useRef, useState } from 'react';
 import { debounce } from 'lodash';
 
 import LoadingPage from '../../pages/loading-page/loading-page';
 
 import { useAppDispatch, useAppSelector } from '../../hooks';
-import { fetchSearchCameraAction } from '../../store/api-actions/api-actions';
+import { fetchCameraByIdAction, fetchSearchCameraAction } from '../../store/api-actions/api-actions';
 import { getSearchedCameras, getSearchedCamerasStatus } from '../../store/catalog-process/catalog-process-selectors';
+import useOnClickOutside from '../../hooks/use-on-click-outside';
 
 import { FetchStatus } from '../../const/fetch-status';
 import { WarningMessage } from '../../const/warning-message';
 import { ErrorMessage } from '../../const/error-message';
 
 import { SEARCH_DELAY } from '../../const/const';
+import { useNavigate } from 'react-router-dom';
+import { AppRoute } from '../../const/app-route';
+import { ComponentName } from '../../const/component-name';
+import { DEFAULT_TABS_TYPE } from '../../const/tabs-buttons';
 
 function FormSearch(): JSX.Element {
   const dispatch = useAppDispatch();
+  const searchRef = useRef(null);
+  const navigate = useNavigate();
 
   const [isDropdownOpened, setDropdownIsOpened] = useState<boolean>(false);
   const [searchValue, setSearchValue] = useState<string>('');
@@ -27,6 +34,11 @@ function FormSearch(): JSX.Element {
   const searchedCameras = useAppSelector(getSearchedCameras);
   const fetchingStatus = useAppSelector(getSearchedCamerasStatus);
 
+  const cleanDropdown = () => {
+    setSearchValue('');
+    setDropdownIsOpened(false);
+  };
+
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     if (!isDropdownOpened) {
       setDropdownIsOpened(true);
@@ -36,10 +48,24 @@ function FormSearch(): JSX.Element {
     debouncedFetchCameras(event.target.value);
   };
 
-  useEffect(() => () => debouncedFetchCameras.cancel());
+  const handleResetButtonClick = () => {
+    cleanDropdown();
+  };
+
+  const handleListItemClick = (event: SyntheticEvent) => {
+    const product = event.target as HTMLInputElement;
+    const productId = Number(product.dataset.id);
+
+    dispatch(fetchCameraByIdAction(productId)).unwrap().then(() => {
+      navigate(`${AppRoute.Product}/${productId}?${ComponentName.Tab}=${DEFAULT_TABS_TYPE}`);
+      cleanDropdown();
+    });
+  };
+
+  useOnClickOutside(searchRef, cleanDropdown);
 
   return(
-    <div className={`form-search ${isDropdownOpened ? 'list-opened' : ''}`}>
+    <div className={`form-search ${isDropdownOpened ? 'list-opened' : ''}`} ref={searchRef}>
       <form>
         <label>
           <svg
@@ -69,13 +95,19 @@ function FormSearch(): JSX.Element {
               tabIndex={0}
               className="form-search__select-item"
               key={item.id}
+              data-id={item.id}
+              onClick={handleListItemClick}
             >
               {item.name}
             </li>
           ))}
         </ul>
       </form>
-      <button className="form-search__reset" type="reset">
+      <button
+        className="form-search__reset"
+        type="reset"
+        onClick={handleResetButtonClick}
+      >
         <svg width={10} height={10} aria-hidden="true">
           <use xlinkHref="#icon-close" />
         </svg>
